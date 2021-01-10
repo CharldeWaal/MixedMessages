f85066fed9d3a4832957960f6906ecb3da5b9973
Charl de Waal <charldewaal8@gmail.com>
2021-01-10 22:31:14 +0200

    final commit

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,1 +1,18 @@
-let words = [];
+let words = {
+    //words to make up randomized sentences
+    start: ['Tomorrow\'s', 'Today\'s', 'Next weekend\'s'],
+    middle: ['conditions are', 'surf is ', 'surf forecast is'],
+    end: ['4-6ft with offshore winds. Cancel your other appointments and go surf!', 'looking great. Get out there!', 'not looking great. Find something else to do.']
+};
+
+const sentenceGenerator = () =>
+{
+    //construct sentence from words object
+    let partOne = words.start[Math.floor(Math.random() * 3)];
+    let partTwo = words.middle[Math.floor(Math.random() * 3)];
+    let partThree = words.end[Math.floor(Math.random() * 3)];
+
+    return partOne + " " + partTwo + " " + partThree;
+}
+
+console.log(sentenceGenerator());
